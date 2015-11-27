aa8fcb77f213d6faa24ebc40c6835cfce861f9f7
Nathan Hall <nathan.mh@gmail.com>
2015-11-27 13:29:06 -0500

    added a node server to get around CORS

--- app/Search/search.services.js
+++ app/Search/search.services.js
@@ -5,12 +5,12 @@
     .factory('SearchFactory', function($http, moment, _) {
 
       //var url = "http://api.bandsintown.com/artists/Dempsey/events.json?api_version=2.0&app_id=findstuff?callback=JSON_CALLBACK";
-      var url = "http://api.bandsintown.com/artists/BraveBaby/events.json?app_id=findstuff?callback=JSON_CALLBACK";
+      var url = "/search";
 
 
 
       var getData = function() {
-        return $http.jsonp(url);
+        return $http.get(url);
       };
 
       return {
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express')
+  , methodOverride = require('method-override')
+  , errorhandler = require('errorhandler')
+  , http    = require('http')
+  , path    = require('path')
+  , bodyParser = require('body-parser')
+  , request = require('request');
+
+// get an instance of express
+var app = express();
+
+
+// Configuration
+  app.set('port', process.env.PORT || 3000);
+  app.set('view engine', 'jade');
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(express.static(path.join(__dirname, 'app')));
+
+// Dev-Specific Configuration
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+  app.use(errorhandler());
+}
+
+// Routes
+app.get('/', express.static(path.join(__dirname, 'app')));
+
+app.get('/search', function(req,res,next){
+  var url = "http://api.bandsintown.com/artists/BraveBaby/events.json?app_id=findstuff?callback=JSON_CALLBACK";
+  request.get({url: url}, function(err, response) {
+    res.send(JSON.parse(response.body));
+  })
+})
+
+// Light It Up!
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
